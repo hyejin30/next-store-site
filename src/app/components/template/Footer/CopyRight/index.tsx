119da85a349869e48a53385729db36ec8469ebbf
hyejin30 <tyui3185@gmail.com>
2023-01-29 17:39:06 +0900

    Fix : 카피라이트 문구 수정

--- src/app/components/template/Footer/CopyRight/index.tsx
+++ src/app/components/template/Footer/CopyRight/index.tsx
@@ -1,7 +1,7 @@
 import { Text } from '@/app/components/atom';
 
 function CopyRight() {
-  return <Text.Small>© 2017 COMMON GROUND.</Text.Small>;
+  return <Text.Small>© 2023 HYEJIN KIM</Text.Small>;
 }
 
 export default CopyRight;
