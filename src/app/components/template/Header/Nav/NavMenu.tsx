8a88acbf265d5102deb5cba6421dd86b8031a90d
hyejin30 <tyui3185@gmail.com>
2023-01-29 17:33:46 +0900

    Fix : NavMenu Kanit 폰트로 변경

--- src/app/components/template/Header/Nav/NavMenu.tsx
+++ src/app/components/template/Header/Nav/NavMenu.tsx
@@ -1,9 +1,8 @@
 import { ReactNode } from 'react';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import styled from 'styled-components';
 
 import { Text } from '@/app/components/atom';
-import { colors } from '@/styles';
 
 interface INavMenuProps {
   children: ReactNode;
@@ -11,15 +10,15 @@ interface INavMenuProps {
 }
 
 function NavMenu({ children, path }: INavMenuProps) {
-  const pathname = usePathname();
-
   return (
     <Link href={path}>
-      <Text.Medium color={pathname === path ? colors.BLUE : colors.BLACK} weight={pathname === path ? 700 : 400}>
-        {children}
-      </Text.Medium>
+      <NavMenuText>{children}</NavMenuText>
     </Link>
   );
 }
 
 export default NavMenu;
+
+const NavMenuText = styled(Text.Medium)`
+  font-family: 'Kanit', sans-serif;
+`;
